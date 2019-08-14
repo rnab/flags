cb7efd4381068b5cfa4efe4825e49397661b5fa4
R N <riknabeshima@gmail.com>
2019-08-14 23:40:54 +0100

    it's working

--- Test.R
+++ Test.R
@@ -5,6 +5,21 @@ a<-image_read("data/ad.png")
 print(a)
 image_info(a)
 ?image_resize
-p<-image_resize(a, geometry_size_pixels(width = 5, height = 5))
+p<-image_resize(a, "5x5!")
+help("image_resize")
 print(p)
 head(p)
+
+x<-p[[1]]
+x[1:2,5,5]
+
+
+library("tidyverse")
+out<-read_csv("out.csv")
+
+names(out)<-c("cn",paste0("v",1:75))
+
+library("class")
+tst=out%>%sample_n(10)
+
+knn(out[,2:76],tst[,2:76],cl=out$cn)
